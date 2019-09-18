fdc6c7e27250f80689786c7c2a953e245855157f
nithinkulal3 <nithinkulal18@gmail.com>
2019-09-18 20:12:15 +0530

    Added ajax for pagination and sorting

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   def index
     sorter = params[:sort].present? ? params[:sort] : 'project_created_at'
     sort_asc_desc = sorter == 'project_created_at' ? 'desc' : 'asc'
-    @projects = Project.paginate(page: params[:page], per_page: 5)
+    @projects = Project.paginate(page: params[:page], per_page: 2)
       .select('projects.title, projects.created_at as project_created_at, users.username, categories.name as category_name')
       .joins(:user, :category).order(sorter => sort_asc_desc)
   end
